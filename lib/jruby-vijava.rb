2b429912d60889bc2c544189f488e7d4797f4c9b
Austin Ziegler <austin@halostatue.ca>
2011-08-04 13:39:57 -0400

    Minor changes before release.

---
+++ lib/jruby-vijava.rb
@@ -0,0 +1,5 @@
+# -*- ruby encoding: utf-8 -*-
+
+require 'vijava'
+
+# vim: ft=ruby
--- lib/vijava.rb
+++ lib/vijava.rb
@@ -1,16 +1,22 @@
+# -*- ruby encoding: utf-8 -*-
 require 'vijava/dom4j-1.6.1.jar'
 require 'vijava/vijava2120100824.jar'
 
+# A wrapper around VIJava's com.vmware.vim25.mo package.
 module VIJava
   VERSION = '2.1.0'
 
   include_package "com.vmware.vim25.mo"
 
+  # A wrapper around VIJava's com.vmware.vim25.mo.util package.
   module Util
     include_package "com.vmware.vim25.mo.util"
   end
 
+  # A wrapper around VIJava's com.vmware.vim25 pacakge.
   module Vim
     include_package "com.vmware.vim25"
   end
 end
+
+# vim: ft=ruby
